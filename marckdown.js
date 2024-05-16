d0fc4c6784bbde98ba4c60a40128b5b6208c882b
sohho888 <sohho888@email.ru>
2024-05-16 11:55:42 +0100

    solution 3 test

--- marckdown.js
+++ marckdown.js
@@ -1,27 +1,31 @@
 export function render(markdown) {
-  if (typeof markdown === 'string') {
+  if (typeof markdown === "string") {
     return markdown;
   }
 
-  let child = markdown.content.map(render).join('');
+  let child = markdown.content.map(render).join("");
 
-  if (markdown.type === 'document') {
+  if (markdown.type === "document") {
     return child;
   }
 
-  if (markdown.type === 'paragraph') {
-    return child + '\n';
+  if (markdown.type === "paragraph") {
+    return child + "\n";
   }
 
-  if (markdown.type === 'heading') {
-    return '#'.child + ' ' + child;
+  if (markdown.type === "heading") {
+    return (
+      "# " +
+      child +
+      (markdown.content.some((item) => item.type !== "heading") ? "\n\n" : "")
+    );
   }
 
-  if (markdown.type === 'bold') {
-    return '**' + child + '**';
+  if (markdown.type === "bold") {
+    return "**" + child + "**";
   }
 
-  if (markdown.type === 'italic') {
-    return '_' + child + '_';
+  if (markdown.type === "italic") {
+    return "_" + child + "_";
   }
 }
--- markdown.test.js
+++ markdown.test.js
@@ -1,5 +1,5 @@
-import { describe, test, expect } from 'vitest';
-import { render } from './marckdown';
+import { describe, test, expect } from "vitest";
+import { render } from "./marckdown";
 
 /**
  * Example:
@@ -8,116 +8,118 @@ import { render } from './marckdown';
  */
 const includeOnly = [];
 
-describe('render', () => {
+describe("render", () => {
   test.each(
     [
       {
         input: {
-          type: 'document',
-          content: ['hello'],
+          type: "document",
+          content: ["hello"],
         },
-        output: 'hello',
+        output: "hello",
       },
       {
         input: {
-          type: 'document',
-          content: [{ type: 'paragraph', content: ['hello'] }],
+          type: "document",
+          content: [{ type: "paragraph", content: ["hello"] }],
         },
-        output: 'hello\n',
+        output: "hello\n",
       },
       {
         input: {
-          type: 'document',
+          type: "document",
           content: [
             {
-              type: 'paragraph',
-              content: [{ type: 'bold', content: ['hello'] }],
+              type: "paragraph",
+              content: [{ type: "bold", content: ["hello"] }],
             },
           ],
         },
-        output: '**hello**\n',
+        output: "**hello**\n",
       },
       {
         input: {
-          type: 'document',
-          content: [{ type: 'heading', content: ['hello'] }],
+          type: "document",
+          content: [{ type: "heading", content: ["hello"] }],
         },
-        output: '# hello\n',
+        output: "# hello\n",
       },
       {
         input: {
-          type: 'document',
+          type: "document",
           content: [
             {
-              type: 'paragraph',
+              type: "paragraph",
               content: [
-                { type: 'bold', content: ['hello'] },
-                { type: 'italic', content: ['world'] },
+                { type: "bold", content: ["hello"] },
+                { type: "italic", content: ["world"] },
               ],
             },
           ],
         },
-        output: '**hello**_world_\n',
+        output: "**hello**_world_\n",
       },
       {
         input: {
-          type: 'document',
+          type: "document",
           content: [
             {
-              type: 'paragraph',
-              content: ['hello'],
+              type: "paragraph",
+              content: ["hello"],
             },
             {
-              type: 'paragraph',
-              content: ['world'],
+              type: "paragraph",
+              content: ["world"],
             },
           ],
         },
-        output: 'hello\nworld\n',
+        output: "hello\nworld\n",
       },
       {
         input: {
-          type: 'document',
+          type: "document",
           content: [
             {
-              type: 'heading',
-              content: ['First section'],
+              type: "heading",
+              content: ["First section"],
             },
             {
-              type: 'paragraph',
+              type: "paragraph",
               content: [
-                'I am content, can be ',
+                "I am content, can be ",
                 {
-                  type: 'bold',
-                  content: ['bold'],
+                  type: "bold",
+                  content: ["bold"],
                 },
-                ' or ',
+                " or ",
                 {
-                  type: 'italic',
-                  content: ['italic'],
+                  type: "italic",
+                  content: ["italic"],
                 },
-                '.',
+                ".",
               ],
             },
             {
-              type: 'heading',
-              content: ['Second section'],
+              type: "heading",
+              content: ["Second section"],
             },
             {
-              type: 'paragraph',
+              type: "paragraph",
               content: ["I'm a simple text, nothing special"],
             },
             {
-              type: 'paragraph',
-              content: ['Yet another paragraph'],
+              type: "paragraph",
+              content: ["Yet another paragraph"],
             },
           ],
         },
         output:
           "# First section\n\nI am content, can be **bold** or _italic_.\n# Second section\n\nI'm a simple text, nothing special\nYet another paragraph\n",
       },
-    ].filter((_, idx) => (includeOnly.length === 0 ? true : includeOnly.includes(idx + 1))),
-  )('%#', ({ input, output }) => {
+    ].filter((_, idx) =>
+      includeOnly.length === 0 ? true : includeOnly.includes(idx + 1),
+    ),
+  )("%#", ({ input, output }) => {
     expect(render(input)).toBe(output);
   });
 });
