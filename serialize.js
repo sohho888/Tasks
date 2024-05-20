e9dc3337911945b743a4d8b84dfe9211d5f384eb
sohho888 <sohho888@email.ru>
2024-05-20 13:16:45 +0100

    add serialize.js

---
+++ serialize.js
@@ -0,0 +1,16 @@
+let result = {};
+export function serialize(node) {
+  if (node === null) {
+    return result.stringify(node);
+  }
+
+  while (node) {
+    if (node.left < node) {
+      serialize(node.left);
+    } else {
+      if (node.right > node) {
+        serialize(node.right);
+      }
+    }
+  }
+}
