2e2e7ee417e85f9e4dcb90022059644b42272368
sohho888 <sohho888@email.ru>
2024-05-25 22:52:23 +0100

    first implementation

---
+++ stirng-valid.js
@@ -0,0 +1,25 @@
+function insertingString(str) {
+  if (str == '') {
+    return 'abc';
+  }
+
+  let strSplit = str.split('');
+  let subStrings = '';
+
+  if (strSplit.length > 2) {
+    for (let i = 0; i < strSplit.length; i++) {
+      let subString = strSplit[i] + strSplit[i + 1] + strSplit[i + 2];
+
+      subStrings += strSplit[i];
+
+      if (subString === 'abc') {
+        strSplit.splice([i], 3);
+        return insertingString(strSplit.join(''));
+      }
+    }
+    return true;
+  }
+  if (strSplit.length === 2) {
+    return false;
+  }
+}
