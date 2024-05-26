2a9164bc95defab681676a467c35781e33d6d7d7
sohho888 <sohho888@email.ru>
2024-05-26 18:43:54 +0100

    fix all the test

--- string-valid.js
+++ string-valid.js
@@ -6,26 +6,24 @@
  */
 export function insertingString(str) {
   if (str == "") {
-    return "abc";
+    return true;
   }
 
   let strSplit = str.split("");
-  let subStrings = "";
 
-  if (strSplit.length > 2) {
+  if (strSplit.length) {
     for (let i = 0; i < strSplit.length; i++) {
       let subString = strSplit[i] + strSplit[i + 1] + strSplit[i + 2];
 
-      subStrings += strSplit[i];
+      if (strSplit.length <= 2) {
+        return false;
+      }
 
       if (subString === "abc") {
-        strSplit.splice([i], 3);
+        strSplit.splice(i, 3);
         return insertingString(strSplit.join(""));
       }
     }
-    return true;
   }
-  if (strSplit.length === 2) {
-    return false;
-  }
+  return false;
 }
